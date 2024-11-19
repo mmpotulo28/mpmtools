c5c2927096dc95ac0f90ccfa93bc11e1b58d60ea
mmpotulo28 <mpotulom28@gmail.com>
2024-11-19 10:44:29 +0200

    Wrap downloadImages function in a check for document availability

--- src/pages/snippets/download-images/index.js
+++ src/pages/snippets/download-images/index.js
@@ -1,30 +1,32 @@
-const downloadImages = async (containerClassName) => {
-	const container = document.querySelector(`.${containerClassName}`);
-	console.log("Container:", container);
-	if (!container) {
-		console.error("Container not found");
-		return;
-	}
+if (typeof document !== "undefined") {
+	const downloadImages = async (containerClassName) => {
+		const container = document.querySelector(`.${containerClassName}`);
+		console.log("Container:", container);
+		if (!container) {
+			console.error("Container not found");
+			return;
+		}
 
-	const images = container.querySelectorAll("img");
-	console.log("Images:", images);
-	for (let i = 0; i < images.length; i++) {
-		const img = images[i];
-		const imageUrl = img.src;
-		const imageName = imageUrl.split("/").pop() || "default_image_name";
+		const images = container.querySelectorAll("img");
+		console.log("Images:", images);
+		for (let i = 0; i < images.length; i++) {
+			const img = images[i];
+			const imageUrl = img.src;
+			const imageName = imageUrl.split("/").pop() || "default_image_name";
 
-		const downloadLink = document.createElement("a");
-		downloadLink.href = imageUrl;
-		downloadLink.download = imageName;
+			const downloadLink = document.createElement("a");
+			downloadLink.href = imageUrl;
+			downloadLink.download = imageName;
 
-		document.body.appendChild(downloadLink);
-		downloadLink.click();
-		document.body.removeChild(downloadLink);
+			document.body.appendChild(downloadLink);
+			downloadLink.click();
+			document.body.removeChild(downloadLink);
 
-		// Add a delay between each download
-		await new Promise((resolve) => setTimeout(resolve, 1000));
-	}
-};
+			// Add a delay between each download
+			await new Promise((resolve) => setTimeout(resolve, 1000));
+		}
+	};
 
-// Example usage:
-downloadImages("mCSB_container");
+	// Example usage:
+	downloadImages("mCSB_container");
+}
